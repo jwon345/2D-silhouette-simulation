9e9f3369a3b3801575d6c83491c4272e9dcc467d
James <59201899+jwon345@users.noreply.github.com>
2023-03-19 02:01:54 +1300

    Led to Sensor Hard coded

--- Program.cs
+++ Program.cs
@@ -15,7 +15,9 @@ namespace ML
         public static void Main()
         {
 
-            int numberOfLED = 0;
+            int numberOfLED = 1;
+
+            int numberOfSensors = 1;
 
             int Radius = 100;
 
@@ -24,9 +26,8 @@ namespace ML
 
             bool drawRays = false;
 
-            int BoxLength = 100;
+            float offset = 0;
 
-            int a = 0;
 
             Raylib.InitWindow(screenWidth, screenHeight, "Ray Simluation");
 
@@ -57,12 +58,43 @@ namespace ML
 
                 Raylib.DrawCircleLines((int)screenWidth / 2, (int)screenHeight / 2, Radius, Color.GREEN);
 
+                for (int k = 0; k < numberOfSensors; k++)
+                {
+
+
+                    float angle = (float)((((float)(k + 1) / (numberOfSensors)))* 2 * Math.PI);
+
+
+                    Raylib.DrawCircle((int)(screenWidth/2 +(Radius * Math.Sin(angle))),(int)(screenHeight/2 + (Radius * Math.Cos(angle))),5, Color.BLACK);
+                    
+
+                  if(drawRays)
+                    {
+                        for (int j = 0; j < numberOfLED; j++)
+                        {
+                            float Circleangle = (float)((((float)(j + 1) / (numberOfLED))) * 2 * Math.PI);
+
+                            Circleangle += offset * (float)(Math.PI / 180);
+
+                            int x = (int)(screenWidth / 2 + (Radius * Math.Sin(Circleangle)));
+                            int y = (int)(screenHeight / 2 + (Radius * Math.Cos(Circleangle)));
+
+                            Raylib.DrawLine(x,y,(int)(screenWidth / 2 + (Radius * Math.Sin(angle))), (int)(screenHeight / 2 + (Radius * Math.Cos(angle))), Color.PINK);
+
+                        }
+                    }
+                }
+
                 for (int i = 0; i < numberOfLED; i++)
                 {
 
-                    float angle = (float)((((float)(i + 1) / (numberOfLED)))* 2 * Math.PI);
+                    float angle = (float)(((float)(i + 1) / (numberOfLED))* 2 * Math.PI);
 
-                    if(drawRays)Radiate.Radiate.drawthis((int)(screenWidth/2 +(Radius * Math.Sin(angle))),(int)(screenHeight/2 + (Radius * Math.Cos(angle))), Color.RED);
+//                    Raylib.DrawCircle((int)(screenWidth/2 +(Radius * Math.Sin(angle))),(int)(screenHeight/2 + (Radius * Math.Cos(angle))),5, Color.BLACK);
+
+                    angle += offset * (float)(Math.PI / 180);
+                    
+//                  if(drawRays)Radiate.Radiate.drawthis((int)(screenWidth/2 +(Radius * Math.Sin(angle))),(int)(screenHeight/2 + (Radius * Math.Cos(angle))), Color.RED);
 
                     Raylib.DrawCircle((int)(screenWidth/2 +(Radius * Math.Sin(angle))),(int)(screenHeight/2 + (Radius * Math.Cos(angle))),10, Color.RED);
                 }
@@ -71,8 +103,8 @@ namespace ML
 
 
                    //controller update
-                controller.Update(dt);
 
+                controller.Update(dt);
 
                 ImGui.Begin("Simulation Settings");
 
@@ -85,19 +117,31 @@ namespace ML
                     circleSelect = !squareSelect;
                 }
 
-                if( ImGui.Checkbox("draw rays", ref drawRays))
-
-
+                ImGui.Checkbox("draw rays", ref drawRays);
+               
+                
 
                 ImGui.Text("Set number of LEDS");
-                ImGui.InputInt("", ref numberOfLED);
+                ImGui.InputInt("i", ref numberOfLED);
+
+
+                ImGui.Text("Set number of Sensors");
+                ImGui.InputInt("b", ref numberOfSensors);
 
 
                 ImGui.Text("Set Circle Radius");
-                ImGui.SliderInt("", ref Radius, 0, 500);
+                ImGui.SliderInt("a", ref Radius, 0, 1000);
 
+                ImGui.Text("Set angle offset");
+                ImGui.SliderFloat("k", ref offset, 1, 360/numberOfLED);
+
+                if( ImGui.Button("Stagger alignment"))
+                {
+                    offset = 180 / numberOfLED;
+                }
 
                 ImGui.End();
+
 
                 controller.Draw();
 
