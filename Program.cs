08e95173baf607a6d8c68a027456f2fcb1d7019c
James <59201899+jwon345@users.noreply.github.com>
2023-04-14 06:29:34 +1200

    added dithering

--- Program.cs
+++ Program.cs
@@ -10,7 +10,19 @@ namespace ML
     {
         const int screenWidth = 1200;
         const int screenHeight = 800;
+        static IDictionary<int, float> ditheringOffsets = new Dictionary<int, float>();
 
+        static void setDitherRandom(int NumberOfLeds, float spacing)
+        {
+            Random random = new Random();
+            for (int i = 0; i < NumberOfLeds; i++)
+            {
+                ditheringOffsets[i] = spacing * (float)random.NextDouble();
+                Console.WriteLine(ditheringOffsets[i]);
+            }
+        }
+
+        
 
         public static void Main()
         {
@@ -21,13 +33,24 @@ namespace ML
 
             int Radius = 100;
 
+            float iterationSpeed = 1;
+            float canIterate = 0;
+
             bool circleSelect = true;
             bool squareSelect = false;
 
+            bool bottleCapDraw = false;
+
             bool drawRays = false;
 
+            bool isDithering = false;
+
+            bool iterateLEDs = false;
+
+               
             float offset = 0;
 
+            int iterationNumber = 0;
 
             Raylib.InitWindow(screenWidth, screenHeight, "Ray Simluation");
 
@@ -70,36 +93,124 @@ namespace ML
 
                   if(drawRays)
                     {
-                        for (int j = 0; j < numberOfLED; j++)
+                        if (iterateLEDs)
                         {
-                            float Circleangle = (float)((((float)(j + 1) / (numberOfLED))) * 2 * Math.PI);
+                                if (!isDithering)
+                                {
+                                        float Circleangle = (float)((((float)(iterationNumber + 1) / (numberOfLED))) * 2 * Math.PI);
 
-                            Circleangle += offset * (float)(Math.PI / 180);
+                                        Circleangle += offset * (float)(Math.PI / 180);
 
-                            int x = (int)(screenWidth / 2 + (Radius * Math.Sin(Circleangle)));
-                            int y = (int)(screenHeight / 2 + (Radius * Math.Cos(Circleangle)));
+                                        int x = (int)(screenWidth / 2 + (Radius * Math.Sin(Circleangle)));
+                                        int y = (int)(screenHeight / 2 + (Radius * Math.Cos(Circleangle)));
 
-                            Raylib.DrawLine(x,y,(int)(screenWidth / 2 + (Radius * Math.Sin(angle))), (int)(screenHeight / 2 + (Radius * Math.Cos(angle))), Color.PINK);
+                                        Raylib.DrawLine(x,y,(int)(screenWidth / 2 + (Radius * Math.Sin(angle))), (int)(screenHeight / 2 + (Radius * Math.Cos(angle))), Color.PINK);
+
+                                }
+                                else
+                                {
+                                        float Circleangle = (float)((((float)(iterationNumber + 1) / (numberOfLED))) * 2 * Math.PI);
+
+                                        Circleangle += offset * (float)(Math.PI / 180);
+                                        Circleangle += ditheringOffsets[iterationNumber];
+
+                                        int x = (int)(screenWidth / 2 + (Radius * Math.Sin(Circleangle)));
+                                        int y = (int)(screenHeight / 2 + (Radius * Math.Cos(Circleangle)));
+
+                                        Raylib.DrawLine(x,y,(int)(screenWidth / 2 + (Radius * Math.Sin(angle))), (int)(screenHeight / 2 + (Radius * Math.Cos(angle))), Color.PINK);
+
+                                }
+
+                            if(Raylib.GetTime() > canIterate)
+                            {
+                                canIterate = (float)Raylib.GetTime() + iterationSpeed;
+                                iterationNumber++;
+                                if (iterationNumber > numberOfLED)
+                                {
+                                    iterationNumber = 0;
+                                }
+                                
+                            }
+                        }
+                        else
+                        {
+                            if (!isDithering)
+                            {
+                                for (int j = 0; j < numberOfLED; j++)
+                                {
+                                    float Circleangle = (float)((((float)(j + 1) / (numberOfLED))) * 2 * Math.PI);
+
+                                    Circleangle += offset * (float)(Math.PI / 180);
+
+                                    int x = (int)(screenWidth / 2 + (Radius * Math.Sin(Circleangle)));
+                                    int y = (int)(screenHeight / 2 + (Radius * Math.Cos(Circleangle)));
+
+                                    Raylib.DrawLine(x,y,(int)(screenWidth / 2 + (Radius * Math.Sin(angle))), (int)(screenHeight / 2 + (Radius * Math.Cos(angle))), Color.PINK);
+
+                                }
+                            }
+                            else
+                            {
+                                for (int j = 0; j < numberOfLED; j++)
+                                {
+                                    float Circleangle = (float)((((float)(j + 1) / (numberOfLED))) * 2 * Math.PI);
+
+                                    Circleangle += offset * (float)(Math.PI / 180);
+                                    Circleangle += ditheringOffsets[j];
+
+                                    int x = (int)(screenWidth / 2 + (Radius * Math.Sin(Circleangle)));
+                                    int y = (int)(screenHeight / 2 + (Radius * Math.Cos(Circleangle)));
+
+                                    Raylib.DrawLine(x,y,(int)(screenWidth / 2 + (Radius * Math.Sin(angle))), (int)(screenHeight / 2 + (Radius * Math.Cos(angle))), Color.PINK);
+
+                                }
+
+                            }
 
                         }
                     }
                 }
 
-                for (int i = 0; i < numberOfLED; i++)
+                if (!isDithering)
                 {
+                    for (int i = 0; i < numberOfLED; i++)
+                    {
 
-                    float angle = (float)(((float)(i + 1) / (numberOfLED))* 2 * Math.PI);
+                        float angle = (float)(((float)(i + 1) / (numberOfLED))* 2 * Math.PI);
 
-//                    Raylib.DrawCircle((int)(screenWidth/2 +(Radius * Math.Sin(angle))),(int)(screenHeight/2 + (Radius * Math.Cos(angle))),5, Color.BLACK);
+    //                    Raylib.DrawCircle((int)(screenWidth/2 +(Radius * Math.Sin(angle))),(int)(screenHeight/2 + (Radius * Math.Cos(angle))),5, Color.BLACK);
 
-                    angle += offset * (float)(Math.PI / 180);
-                    
-//                  if(drawRays)Radiate.Radiate.drawthis((int)(screenWidth/2 +(Radius * Math.Sin(angle))),(int)(screenHeight/2 + (Radius * Math.Cos(angle))), Color.RED);
+                        angle += offset * (float)(Math.PI / 180);
+                        
+    //                  if(drawRays)Radiate.Radiate.drawthis((int)(screenWidth/2 +(Radius * Math.Sin(angle))),(int)(screenHeight/2 + (Radius * Math.Cos(angle))), Color.RED);
 
-                    Raylib.DrawCircle((int)(screenWidth/2 +(Radius * Math.Sin(angle))),(int)(screenHeight/2 + (Radius * Math.Cos(angle))),10, Color.RED);
+                        Raylib.DrawCircle((int)(screenWidth/2 +(Radius * Math.Sin(angle))),(int)(screenHeight/2 + (Radius * Math.Cos(angle))),10, Color.RED);
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < numberOfLED; i++)
+                    {
+
+                        float angle = (float)(((float)(i + 1) / (numberOfLED))* 2 * Math.PI);
+
+    //                    Raylib.DrawCircle((int)(screenWidth/2 +(Radius * Math.Sin(angle))),(int)(screenHeight/2 + (Radius * Math.Cos(angle))),5, Color.BLACK);
+
+                        angle += offset * (float)(Math.PI / 180);
+                        angle += ditheringOffsets[i];
+                        
+    //                  if(drawRays)Radiate.Radiate.drawthis((int)(screenWidth/2 +(Radius * Math.Sin(angle))),(int)(screenHeight/2 + (Radius * Math.Cos(angle))), Color.RED);
+
+                        Raylib.DrawCircle((int)(screenWidth/2 +(Radius * Math.Sin(angle))),(int)(screenHeight/2 + (Radius * Math.Cos(angle))),10, Color.RED);
+                    }
                 }
 
 
+                
+                if (bottleCapDraw)
+                {
+                        Raylib.DrawCircle((int)Raylib.GetMousePosition().X,(int)Raylib.GetMousePosition().Y,(float)25, Color.RED);
+                }
 
 
                    //controller update
@@ -116,6 +227,10 @@ namespace ML
                 {
                     circleSelect = !squareSelect;
                 }
+                if( ImGui.Checkbox("Mouse = BottleCap (25mmRadius)", ref bottleCapDraw))
+                {
+
+                }
 
                 ImGui.Checkbox("draw rays", ref drawRays);
                
@@ -130,7 +245,7 @@ namespace ML
 
 
                 ImGui.Text("Set Circle Radius");
-                ImGui.SliderInt("a", ref Radius, 0, 1000);
+                ImGui.SliderInt("mm Equivalent", ref Radius, 0, 1000);
 
                 ImGui.Text("Set angle offset");
                 ImGui.SliderFloat("k", ref offset, 1, 360/numberOfLED);
@@ -138,6 +253,25 @@ namespace ML
                 if( ImGui.Button("Stagger alignment"))
                 {
                     offset = 180 / numberOfLED;
+                }
+
+                if( ImGui.Button("Random Dither spacing"))
+                {
+                    isDithering = true;
+                    float angle = (float)(((float)(1) / (numberOfLED))* 2 * Math.PI);
+                    setDitherRandom(numberOfLED, angle);
+                }
+
+                if( ImGui.Button("Equal Spacing"))
+                {
+                    isDithering = false;
+                }
+
+                ImGui.Checkbox("Iterate Through LEDS", ref iterateLEDs);
+                if (iterateLEDs)
+                {
+                    ImGui.Text("iteration Speed");
+                    ImGui.SliderFloat("cycle/s", ref iterationSpeed, 0.1f, 2);
                 }
 
                 ImGui.End();
@@ -152,6 +286,8 @@ namespace ML
             controller.Dispose();
             Raylib.CloseWindow();
         }
+
+
 
     }
 
